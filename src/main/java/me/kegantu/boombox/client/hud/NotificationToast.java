90dea64496573038aa3845b1a5f3e4f436386aab
Kegantu <90145340+Kegantu@users.noreply.github.com>
2025-12-13 23:24:39 +0200

    Added dics texture and trying to figure out how to make it spin properly

--- src/main/java/me/kegantu/boombox/client/hud/NotificationToast.java
+++ src/main/java/me/kegantu/boombox/client/hud/NotificationToast.java
@@ -1,5 +1,6 @@
 package me.kegantu.boombox.client.hud;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import me.kegantu.boombox.BoomBox;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -9,11 +10,14 @@ import net.minecraft.client.toast.ToastManager;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.StringVisitable;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
 
 @Environment(EnvType.CLIENT)
 public class NotificationToast implements Toast {
 
-    private final Identifier NOTIFICATIONS_TEXTURE = new Identifier(BoomBox.MOD_ID, "textures/gui/notifications40v2.png");
+    private final Identifier NOTIFICATIONS_TEXTURE = new Identifier(BoomBox.MOD_ID, "textures/gui/notifications40v3.png");
+    //private final Identifier MUSIC_DISC_TEXTURE = new Identifier(BoomBox.MOD_ID, "textures/gui/musicdisc.png");
     private final String soundtrackName;
 
     public NotificationToast(String soundtrackName) {
@@ -23,18 +27,22 @@ public class NotificationToast implements Toast {
     @Override
     public Visibility draw(DrawContext context, ToastManager manager, long startTime) {
         MatrixStack stack = context.getMatrices();
+        RenderSystem.enableBlend();
         context.drawTexture(NOTIFICATIONS_TEXTURE, 0,0,0,0, this.getWidth(), this.getHeight());
+        stack.push();
+        stack.translate(10,20,0f);
+        stack.scale(1.6f,1.6f,1.6f);
+        stack.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(MathHelper.lerp(0.5f, 0f, 180f)));
+        context.drawTexture(NOTIFICATIONS_TEXTURE, 0,0,177,2, 20, 20);
+        stack.pop();
 
-        //stack.push();
-        //stack.translate(30f,7f, 0f);
-        //stack.scale(0.75f, 0.75f, 0.75f);
         var textLine = manager.getClient().textRenderer.wrapLines(StringVisitable.plain(soundtrackName), 145);
         int y = 7;
         for (int i = 0; i < textLine.size(); i++) {
             context.drawText(manager.getClient().textRenderer, textLine.get(i), 34, y, 1947988, false);
             y += 10;
         }
-        //stack.pop();
+        //context.drawTexture(MUSIC_DISC_TEXTURE, 34, 40, 0, 0, 18, 18);
 
         return startTime >= 7500.0 * manager.getNotificationDisplayTimeMultiplier() ? Toast.Visibility.HIDE : Toast.Visibility.SHOW;
     }
